557353bade6d77faf0ba895e6cc1c9c47780b146
Gustavo F Silva <gustavofsilva.dev@gmail.com>
2018-06-03 13:18:53 -0300

    Melhorando ambiente de desenvolvimento

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 /* Importando plugins para facilitar o desenvolvimento */
-var gulp = require('gulp');
-var pug = require('gulp-pug');
+var gulp = require('gulp'); // core do gulp
+var pug = require('gulp-pug'); // plugin para trabalhar com arquivos PUG
+var uglify = require('gulp-uglify'); // plugin para mimificação de arquivos
+var rename = require('gulp-rename'); // plugin para trabalhar com nomes de arquivos
+var clean = require('gulp-clean'); // plugin para trabalhar com exclusão de arquivos e diretórios
 
 /* Tarefa para compilar os arquivos PUG para HTML */
 gulp.task('pug', function(){
@@ -14,9 +17,18 @@ gulp.task('pug-watch', function(){
 });
 /* Tarefa para compilar ou mover todas as dependências JS nescessárias */
 gulp.task('dist-js', function(){
-    // Move a API Cytoscape para a a área de deploy
+    // Move a API Cytoscape para a área de deploy
     gulp.src('./node_modules/cytoscape/dist/cytoscape.min.js')
         .pipe(gulp.dest('./dist/js'));
+    // Move, mimifica e renomeia os arquivos JS de desenvolvimento para a área de deploy 
+    gulp.src('./dev/js/*.js')
+        .pipe(uglify())
+        .pipe(rename({ extname: ".min.js"}))
+        .pipe(gulp.dest('./dist/js'));
+});
+/* Tarefa que verifica e compila se houve alguma modificações nos arquivos JS */
+gulp.task('js-watch', function(){
+    gulp.watch('dev/js/*', ['dist-js']);
 });
 
 
@@ -24,4 +36,14 @@ gulp.task('dist-js', function(){
 /* Tarefa padrão, quando apenas se digita 'gulp' no terminal */
 gulp.task('default', ['dist-js', 'pug']);
 /* Tarefa que verifica e toma ações caso houver alguma mudança */
-gulp.task('watch', ['pug-watch']);
+gulp.task('watch', ['js-watch', 'pug-watch']);
+/* Tarefa para excluir pasta de deploy */
+gulp.task('clean', function(){
+    gulp.src('./dist')
+        .pipe(clean());
+});
+/* Tarefa para excluir pasta de deploy e depêndencias do NPM */
+gulp.task('full-clean', function(){
+    gulp.src(['./dist', './node_modules'])
+        .pipe(clean());
+});
